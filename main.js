055b0f7a5d64098d4dea9d072b1219bb571da454
pedroHenriqueBarbosa08 <pedro.ramalho.barbosa@escola.pr.gov.br>
2026-07-28 17:29:25 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -22,17 +22,39 @@ function aumentaTamanho(){
     geraSenha(); 
 }
 const campoSenha = document.querySelector('#campo-senha')
+const checkbox = document.querySelectorAll('.checkbox')
 
+for (i=0 < checkbox.lenght;i++) {
+    checkbox[i].onclick = geraSenha 
+
+}
 const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz'
+const simbolos = '@#$%&*'
+const numeros ='1234567890'
 geraSenha(); 
 
 
 function geraSenha(){
+    let alfabeto ='';
+    if (checkbox[0].checked) {
+    alfabeto = alfabeto + letrasMaiusculas;
+}
+if (checkbox[1].checked) {
+    alfabeto = alfabeto + letrasMinusculas;
+}
+if (checkbox[2].checked) {
+    alfabeto = alfabeto + numeros;
+}
+if (checkbox[3].checked) {
+    alfabeto = alfabeto + simbolos;
+}
+
     let senha = '';
     for (let i = 0; i < tamanhoSenha;i++){
-    let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+    let numeroAleatorio = Math.random()*alfabeto.length;
     numeroAleatorio = math.floor(numeroAleatorip)
-    senha = senha + letrasMaiusculas[numeroAleatorio]
+    senha = senha + alfabeto [numeroAleatorio]
 }
 campoSenha.value = senha; 
  }
